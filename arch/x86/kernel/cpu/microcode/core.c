bfc1aba2484768e35d40d1701652e08f1b7e9e8d
Luis R. Rodriguez <mcgrof@kernel.org>
2016-04-28 22:02:22 -0700

    firware: kill built-in firmware support
    Built in firmware is no longer needed, given if you need
    firmware outside of Linux it can be stuffed into the
    initramfs.
    
    Tested through 0-day, and separately with allyesconfig.
    
    Suggested-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- arch/x86/kernel/cpu/microcode/core.c
+++ arch/x86/kernel/cpu/microcode/core.c
@@ -92,25 +92,6 @@ static bool __init check_loader_disabled_bsp(void)
 	return *res;
 }
 
-extern struct builtin_fw __start_builtin_fw[];
-extern struct builtin_fw __end_builtin_fw[];
-
-bool get_builtin_firmware(struct cpio_data *cd, const char *name)
-{
-#ifdef CONFIG_FW_LOADER
-	struct builtin_fw *b_fw;
-
-	for (b_fw = __start_builtin_fw; b_fw != __end_builtin_fw; b_fw++) {
-		if (!strcmp(name, b_fw->name)) {
-			cd->size = b_fw->size;
-			cd->data = b_fw->data;
-			return true;
-		}
-	}
-#endif
-	return false;
-}
-
 void __init load_ucode_bsp(void)
 {
 	int vendor;
