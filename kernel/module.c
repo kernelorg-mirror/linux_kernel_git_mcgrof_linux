691ead04f0cd7f18d5b5e06d2913ce7dcf866ff7
Luis R. Rodriguez <mcgrof@kernel.org>
2016-12-03 06:41:54 -0800

    module: fix memory leak on early load_module() failures
    While looking for early possible module loading failures I was
    able to reproduce a memory leak possible with kmemleak. There
    are a few rare ways to trigger a failure:
    
      o we've run into a failure while processing kernel parameters
        (parse_args() returns an error)
      o mod_sysfs_setup() fails
      o we're a live patch module and copy_module_elf() fails
    
    Chances of running into this issue is really low.
    
    kmemleak splat:
    
    unreferenced object 0xffff9f2c4ada1b00 (size 32):
      comm "kworker/u16:4", pid 82, jiffies 4294897636 (age 681.816s)
      hex dump (first 32 bytes):
        6d 65 6d 73 74 69 63 6b 30 00 00 00 00 00 00 00  memstick0.......
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
      backtrace:
        [<ffffffff8c6cfeba>] kmemleak_alloc+0x4a/0xa0
        [<ffffffff8c200046>] __kmalloc_track_caller+0x126/0x230
        [<ffffffff8c1bc581>] kstrdup+0x31/0x60
        [<ffffffff8c1bc5d4>] kstrdup_const+0x24/0x30
        [<ffffffff8c3c23aa>] kvasprintf_const+0x7a/0x90
        [<ffffffff8c3b5481>] kobject_set_name_vargs+0x21/0x90
        [<ffffffff8c4fbdd7>] dev_set_name+0x47/0x50
        [<ffffffffc07819e5>] memstick_check+0x95/0x33c [memstick]
        [<ffffffff8c09c893>] process_one_work+0x1f3/0x4b0
        [<ffffffff8c09cb98>] worker_thread+0x48/0x4e0
        [<ffffffff8c0a2b79>] kthread+0xc9/0xe0
        [<ffffffff8c6dab5f>] ret_from_fork+0x1f/0x40
        [<ffffffffffffffff>] 0xffffffffffffffff
    
    Cc: stable@kernel.org
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- kernel/module.c
+++ kernel/module.c
@@ -3709,6 +3709,7 @@ static int load_module(struct load_info *info, const char __user *uargs,
  sysfs_cleanup:
 	mod_sysfs_teardown(mod);
  coming_cleanup:
+	destroy_params(mod->kp, mod->num_kp);
 	blocking_notifier_call_chain(&module_notify_list,
 				     MODULE_STATE_GOING, mod);
 	klp_module_going(mod);
