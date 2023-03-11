3ed493a709586151d444589ad9908ac1b22b34ed
Luis Chamberlain <mcgrof@kernel.org>
2023-03-10 20:00:26 -0800

    module: move check_modinfo() early to early_mod_check()
    This moves check_modinfo() to early_mod_check(). This
    doesn't make any functional changes either, as check_modinfo()
    was the first call on layout_and_allocate(), so we're just
    moving it back one routine and at the end.
    
    This let's us keep separate the checkers from the allocater.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -2284,10 +2284,6 @@ static struct module *layout_and_allocate(struct load_info *info, int flags)
 	unsigned int ndx;
 	int err;
 
-	err = check_modinfo(info->mod, info, flags);
-	if (err)
-		return ERR_PTR(err);
-
 	/* Allow arches to frob section contents and sizes.  */
 	err = module_frob_arch_sections(info->hdr, info->sechdrs,
 					info->secstrings, info->mod);
@@ -2702,7 +2698,11 @@ static int early_mod_check(struct load_info *info, int flags)
 
 	/* Check module struct version now, before we try to use module. */
 	if (!check_modstruct_version(info, info->mod))
-		return ENOEXEC;
+		return -ENOEXEC;
+
+	err = check_modinfo(info->mod, info, flags);
+	if (err)
+		return err;
 
 	return 0;
 }
