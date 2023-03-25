b9fd63710223ea979496debd04c477c3b6f9c4b5
Luis Chamberlain <mcgrof@kernel.org>
2023-03-24 18:28:35 -0700

    sempahore: add a helper for a concurrency limiter
    While I looked at re-using the old kernel/kmod.c (now kernel/module/kmod.c)
    concurrency delimiter methodology for another place in the kernel Linus
    noted that this could be simply replaced with a sempahore [0].
    
    So add that so we we don't re-invent the wheel and make it obvious to use.
    
    While at it, add some tribal knowledge about why some existing binary
    semaphores are not converted to mutexes today.
    
    [0] https://lore.kernel.org/all/CAHk-=whkj6=wyi201JXkw9iT_eTUTsSx+Yb9d4OgmZFjDJA18g@mail.gmail.com/
    
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- include/linux/semaphore.h
+++ include/linux/semaphore.h
@@ -25,8 +25,17 @@ struct semaphore {
 	.wait_list	= LIST_HEAD_INIT((name).wait_list),		\
 }
 
+/*
+ * There is a big difference between a binary semaphore and a mutex.
+ * You cannot call mutex_unlock() from IRQ context because it takes an
+ * internal mutex spin_lock in a non-IRQ-safe manner. Both try_lock()
+ * and unlock() can be called from IRQ context.
+ */
 #define DEFINE_SEMAPHORE(name)	\
 	struct semaphore name = __SEMAPHORE_INITIALIZER(name, 1)
+
+#define CONCURRENCY_LIMITER(name, n) \
+	struct semaphore name = __SEMAPHORE_INITIALIZER(name, n)
 
 static inline void sema_init(struct semaphore *sem, int val)
 {
