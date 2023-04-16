1a47e8512eceac1992697c1cc726d5c2b74e182f
Luis Chamberlain <mcgrof@kernel.org>
2023-04-16 15:24:00 -0700

    dm bufio: simplify by using PAGE_SECTORS_SHIFT
    The PAGE_SHIFT - SECTOR_SHIFT constant be replaced with PAGE_SECTORS_SHIFT
    defined in linux/blt_types.h, which is included by linux/blkdev.h.
    
    This produces no functional changes.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- drivers/md/dm-bufio.c
+++ drivers/md/dm-bufio.c
@@ -426,7 +426,7 @@ static void *alloc_buffer_data(struct dm_bufio_client *c, gfp_t gfp_mask,
 	    gfp_mask & __GFP_NORETRY) {
 		*data_mode = DATA_MODE_GET_FREE_PAGES;
 		return (void *)__get_free_pages(gfp_mask,
-						c->sectors_per_block_bits - (PAGE_SHIFT - SECTOR_SHIFT));
+						c->sectors_per_block_bits - (PAGE_SECTORS_SHIFT));
 	}
 
 	*data_mode = DATA_MODE_VMALLOC;
@@ -464,7 +464,7 @@ static void free_buffer_data(struct dm_bufio_client *c,
 
 	case DATA_MODE_GET_FREE_PAGES:
 		free_pages((unsigned long)data,
-			   c->sectors_per_block_bits - (PAGE_SHIFT - SECTOR_SHIFT));
+			   c->sectors_per_block_bits - (PAGE_SECTORS_SHIFT));
 		break;
 
 	case DATA_MODE_VMALLOC:
@@ -642,7 +642,7 @@ static void use_bio(struct dm_buffer *b, enum req_op op, sector_t sector,
 	unsigned int vec_size, len;
 
 	vec_size = b->c->block_size >> PAGE_SHIFT;
-	if (unlikely(b->c->sectors_per_block_bits < PAGE_SHIFT - SECTOR_SHIFT))
+	if (unlikely(b->c->sectors_per_block_bits < PAGE_SECTORS_SHIFT))
 		vec_size += 2;
 
 	bio = bio_kmalloc(vec_size, GFP_NOWAIT | __GFP_NORETRY | __GFP_NOWARN);
