7847f035a9e09ed77f3cc573731f92ceb4477200
Luis Chamberlain <mcgrof@kernel.org>
2023-09-13 22:19:21 +0200

    filemap: align the index to mapping_min_order in __filemap_add_folio()
    Align the index to the mapping_min_order number of pages while setting
    the XA_STATE and xas_set_order().
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -862,7 +862,10 @@ EXPORT_SYMBOL_GPL(replace_page_cache_folio);
 noinline int __filemap_add_folio(struct address_space *mapping,
 		struct folio *folio, pgoff_t index, gfp_t gfp, void **shadowp)
 {
-	XA_STATE(xas, &mapping->i_pages, index);
+	unsigned int min_order = mapping_min_folio_order(mapping);
+	unsigned int nr_of_pages = (1U << min_order);
+	pgoff_t rounded_index = round_down(index, nr_of_pages);
+	XA_STATE(xas, &mapping->i_pages, rounded_index);
 	int huge = folio_test_hugetlb(folio);
 	bool charged = false;
 	long nr = 1;
@@ -873,11 +876,11 @@ noinline int __filemap_add_folio(struct address_space *mapping,
 
 	if (!huge) {
 		int error = mem_cgroup_charge(folio, NULL, gfp);
-		VM_BUG_ON_FOLIO(index & (folio_nr_pages(folio) - 1), folio);
+		VM_BUG_ON_FOLIO(rounded_index & (folio_nr_pages(folio) - 1), folio);
 		if (error)
 			return error;
 		charged = true;
-		xas_set_order(&xas, index, folio_order(folio));
+		xas_set_order(&xas, rounded_index, folio_order(folio));
 		nr = folio_nr_pages(folio);
 	}
 
@@ -915,6 +918,7 @@ noinline int __filemap_add_folio(struct address_space *mapping,
 			}
 		}
 
+		VM_BUG_ON_FOLIO(folio_order(folio) < min_order, folio);
 		xas_store(&xas, folio);
 		if (xas_error(&xas))
 			goto unlock;
