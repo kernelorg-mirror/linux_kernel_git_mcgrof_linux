6060ec8de0b0cbba99813d1ae97d8d5f201850e7
Luis Chamberlain <mcgrof@kernel.org>
2025-04-28 15:34:47 -0700

    nvme: add awun / nawun sanity check
    AWUN/NAWUN control the atomicity of command execution in relation to
    other commands. They impose inter-command serialization of writing of
    blocks of data to the NVM and prevent blocks of data ending up on the
    NVM containing partial data from one new command and partial data from
    one or more other new commands.
    
    Parse awun / nawun to verify at least the physical block size
    exposed is not greater than this value.
    
    The special case of awun / nawun == 0xffff tells us we can ramp
    up to mdts.
    
    Suggested-by: Dan Helmick <dan.helmick@samsung.com>
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -1999,7 +1999,7 @@ static void nvme_configure_metadata(struct nvme_ctrl *ctrl,
 
 static void nvme_update_atomic_write_disk_info(struct nvme_ns *ns,
 			struct nvme_id_ns *id, struct queue_limits *lim,
-			u32 bs, u32 atomic_bs)
+			u32 bs, u32 atomic_bs, u32 awun_bs)
 {
 	unsigned int boundary = 0;
 
@@ -2007,6 +2007,12 @@ static void nvme_update_atomic_write_disk_info(struct nvme_ns *ns,
 		if (le16_to_cpu(id->nabspf))
 			boundary = (le16_to_cpu(id->nabspf) + 1) * bs;
 	}
+
+	if (awun_bs) {
+		atomic_bs = min(atomic_bs, awun_bs);
+		boundary = min(boundary, awun_bs);
+	}
+
 	lim->atomic_write_hw_max = atomic_bs;
 	lim->atomic_write_hw_boundary = boundary;
 	lim->atomic_write_hw_unit_min = bs;
@@ -2038,6 +2044,7 @@ static bool nvme_update_disk_info(struct nvme_ns *ns, struct nvme_id_ns *id,
 	u32 bs = 1U << head->lba_shift;
 	u32 atomic_bs, phys_bs, io_opt = 0;
 	bool valid = true;
+	u32 awun = 0, awun_bs = 0;
 
 	/*
 	 * The block layer can't support LBA sizes larger than the page size
@@ -2051,6 +2058,16 @@ static bool nvme_update_disk_info(struct nvme_ns *ns, struct nvme_id_ns *id,
 
 	atomic_bs = phys_bs = bs;
 	if (id->nabo == 0) {
+		if (id->nsfeat & NVME_NS_FEAT_ATOMICS && id->nawun)
+			awun = (1 + le16_to_cpu(id->nawun));
+		else
+			awun = (1 + ns->ctrl->subsys->awun);
+		/* Indicates MDTS can be used */
+		if (awun == 0xffff)
+			awun_bs = ns->ctrl->max_hw_sectors << SECTOR_SHIFT;
+		else
+			awun_bs = awun * bs;
+
 		/*
 		 * Bit 1 indicates whether NAWUPF is defined for this namespace
 		 * and whether it should be used instead of AWUPF. If NAWUPF ==
@@ -2061,7 +2078,8 @@ static bool nvme_update_disk_info(struct nvme_ns *ns, struct nvme_id_ns *id,
 		else
 			atomic_bs = (1 + ns->ctrl->subsys->awupf) * bs;
 
-		nvme_update_atomic_write_disk_info(ns, id, lim, bs, atomic_bs);
+		nvme_update_atomic_write_disk_info(ns, id, lim, bs, atomic_bs,
+						   awun_bs);
 	}
 
 	if (id->nsfeat & NVME_NS_FEAT_IO_OPT) {
@@ -2071,6 +2089,14 @@ static bool nvme_update_disk_info(struct nvme_ns *ns, struct nvme_id_ns *id,
 		if (id->nows)
 			io_opt = bs * (1 + le16_to_cpu(id->nows));
 	}
+
+	/*
+	 * npwg and nows could be > awun, in such cases users should be aware of
+	 * out of order reads/writes as npwg and nows are purely performance
+	 * optimizations.
+	 */
+	if (awun)
+		phys_bs = min(awun_bs, phys_bs);
 
 	/*
 	 * Linux filesystems assume writing a single physical block is
@@ -3031,6 +3057,7 @@ static int nvme_init_subsystem(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
 		kfree(subsys);
 		return -EINVAL;
 	}
+	subsys->awun = le16_to_cpu(id->awun);
 	subsys->awupf = le16_to_cpu(id->awupf);
 	nvme_mpath_default_iopolicy(subsys);
 
