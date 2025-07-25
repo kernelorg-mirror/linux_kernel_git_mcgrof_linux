f229eb84613087d77c04907f927703025096ef0e
Kundan Kumar <kundan.kumar@samsung.com>
2025-07-25 21:24:07 +0530

    writeback: add infra for parallel writeback
    This is a prep patch which introduces a new bdi_writeback_ctx structure
    that enables us to have multiple writeback contexts for parallel
    writeback. Each bdi now can have multiple writeback contexts, with each
    writeback context having has its own cgwb tree.
    
    Modify all the functions/places that operate on bdi's wb, wb_list,
    cgwb_tree, wb_switch_rwsem, wb_waitq as these fields have now been moved
    to bdi_writeback_ctx.
    
    This patch mechanically replaces bdi->wb to bdi->wb_ctx[0]->wb and there
    is no functional change.
    
    Signed-off-by: Anuj Gupta <anuj20.g@samsung.com>
    Signed-off-by: Kundan Kumar <kundan.kumar@samsung.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1794,8 +1794,8 @@ static void fuse_writepage_finish(struct fuse_writepage_args *wpa)
 		 * contention and noticeably improves performance.
 		 */
 		folio_end_writeback(ap->folios[i]);
-		dec_wb_stat(&bdi->wb, WB_WRITEBACK);
-		wb_writeout_inc(&bdi->wb);
+		dec_wb_stat(&bdi->wb_ctx[0]->wb, WB_WRITEBACK);
+		wb_writeout_inc(&bdi->wb_ctx[0]->wb);
 	}
 
 	wake_up(&fi->page_waitq);
@@ -1989,7 +1989,7 @@ static void fuse_writepage_args_page_fill(struct fuse_writepage_args *wpa, struc
 	ap->descs[folio_index].offset = 0;
 	ap->descs[folio_index].length = folio_size(folio);
 
-	inc_wb_stat(&inode_to_bdi(inode)->wb, WB_WRITEBACK);
+	inc_wb_stat(&inode_to_bdi(inode)->wb_ctx[0]->wb, WB_WRITEBACK);
 }
 
 static struct fuse_writepage_args *fuse_writepage_args_setup(struct folio *folio,
